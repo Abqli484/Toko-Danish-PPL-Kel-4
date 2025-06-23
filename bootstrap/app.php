4ab108b72feb62cc74313011d337b70b4ec34aec
Abqli484 <abidawaqli@gmail.com>
2025-06-23 23:44:30 +0700

    bangbingbung

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
 
             
         ]);
+        
+        $middleware->validateCsrfTokens(except: [
+        ]);
 
     //     $middleware->trustProxies(at: [
     //     '0.0.0.0/0',
